c3487f7804e67633b730cfc686da0af754b4ae4e
Odinokun <odinokun@gmail.com>
2022-08-23 16:53:34 +0200

    did dop monday

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from './components/Button';
+import {json} from 'stream/consumers';
+
+type TodoType = {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
 
 function App() {
+  const [get, setGet] = useState<Array<TodoType>>([]);
+
+  const getRequestHandler = () => {
+    setGet([]);
+  }
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setGet(json))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        name={'Clean page'}
+        callBack={getRequestHandler}
+      />
+      <ul>
+        {
+          get.map(i => {
+            return (
+              <li key={i.id}>
+                <span>{i.id}</span>
+                <input type="checkbox" checked={i.completed}/>
+                <span>{i.title}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
